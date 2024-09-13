d84ec16555b313850d5aeeb7a0b9c8baf7de1c66
ASHI17092003 <162867819+ASHI17092003@users.noreply.github.com>
2024-09-13 08:43:57 +0000

    datatypes

---
+++ 01_bic/02_DType.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as a newer version
+
+// alert(3+3) we are using nodejs ,not a browser ya console
+
+let name="ashi"
+let age=20
+let isLoggedIn= false
+
+//     NOTE
+//     Number-> 2 to the power 53
+//     BigInt
+//     string->""
+//     Boolean-> true/false
+//     null-> standalon value  console.log(typeof null) // object
+//     undefined; koi value assign nhi kri h
+//     symbol-> unique
